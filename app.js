ca7b8a12c77864c44502c19fd1c76c20e756bb5e
nekoninngen <sa2im3oc@gmail.com>
2024-12-02 14:31:11 +0900

    Update app.js

--- app.js
+++ app.js
@@ -29,5 +29,48 @@
                 result = "この先右方向です";
             }
 
+const prefecturalCapital = [
+    {
+      id: "01",
+      question: "北海道の県庁所在地は？",
+      answer01: "札幌",
+      answer02: "福島",
+      answer03: "前橋",
+      answer04: "秋田",
+    },
+    {
+      id: "02",
+      question: "青森県の県庁所在地は？",
+      answer01: "青森",
+      answer02: "前橋",
+      answer03: "秋田",
+      answer04: "札幌",
+    },
+    {
+      id: "03",
+      question: "岩手県の県庁所在地は？",
+      answer01: "盛岡",
+      answer02: "福島",
+      answer03: "仙台",
+      answer04: "山形",
+    },
+    {
+      id: "04",
+      question: "宮城県の県庁所在地は？",
+      answer01: "仙台",
+      answer02: "札幌",
+      answer03: "前橋",
+      answer04: "水戸",
+    },
+    {
+      id: "05",
+      question: "秋田県の県庁所在地は？",
+      answer01: "秋田",
+      answer02: "盛岡",
+      answer03: "青森",
+      answer04: "札幌",
+    },
+  ];
+                
             document.getElementById('result-output').innerText = "【" + result + "】";
         }
