c5864324d3f70a0409e19677fd8ad5490a334c60
nekoninngen <sa2im3oc@gmail.com>
2024-12-02 14:07:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,26 +3,26 @@
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "凶"; // デフォルトの値
 
-            if (colour === "黒" && number === 0) {
-                result = "小吉";
+            if (colour === "とううん" && number === 0) {
+                result = "残念…";
             } else if (colour === "黒" && number === 1) {
                 result = "中吉";
             } else if (colour === "黒" && number === 2) {
                 result = "凶";
-            } else if (colour === "白" && number === 0) {
-                result = "中吉";
+            } else if (colour === "しののめ" && number === 0) {
+                result = "正解！！";
             } else if (colour === "白" && number === 1) {
                 result = "中吉";
             } else if (colour === "白" && number === 2) {
                 result = "大凶";
-            } else if (colour === "オレンジ" && number === 0) {
-                result = "凶";
+            } else if (colour === "ひがしくも" && number === 0) {
+                result = "ざんねん…";
             } else if (colour === "オレンジ" && number === 1) {
                 result = "大吉";
             } else if (colour === "オレンジ" && number === 2) {
                 result = "中吉";
-            } else if (colour === "青" && number === 0) {
-                result = "あなた今楽しいですか？";
+            } else if (colour === "とうのめ" && number === 0) {
+                result = "惜しい！";
             } else if (colour === "青" && number === 1) {
                 result = "とても素晴らしいことです";
             } else if (colour === "青" && number === 2) {
