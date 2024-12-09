39864954dc52146740f15ee06d796a7edbcaa2d7
nekoninngen <sa2im3oc@gmail.com>
2024-12-09 13:25:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,29 +5,13 @@
 
             if (colour === "とううん" && number === 0) {
                 result = "残念…";
-            } else if (colour === "黒" && number === 1) {
-                result = "中吉";
-            } else if (colour === "黒" && number === 2) {
-                result = "凶";
             } else if (colour === "しののめ" && number === 0) {
                 result = "正解！！";
-            } else if (colour === "白" && number === 1) {
-                result = "中吉";
-            } else if (colour === "白" && number === 2) {
-                result = "大凶";
             } else if (colour === "ひがしくも" && number === 0) {
                 result = "ざんねん…";
-            } else if (colour === "オレンジ" && number === 1) {
-                result = "大吉";
-            } else if (colour === "オレンジ" && number === 2) {
-                result = "中吉";
             } else if (colour === "とうのめ" && number === 0) {
                 result = "惜しい！";
-            } else if (colour === "青" && number === 1) {
-                result = "とても素晴らしいことです";
-            } else if (colour === "青" && number === 2) {
-                result = "この先右方向です";
-            }
+            } else 
 
 const prefecturalCapital = [
     {
