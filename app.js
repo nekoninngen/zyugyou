df303d59ba30bdc09720206d87274ed95ab0adfa
nekoninngen <sa2im3oc@gmail.com>
2024-12-09 13:29:10 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,112 +3,17 @@
             var number = parseInt(document.getElementById('number-input').value, 10);
             var result = "回答してね"; // デフォルトの値
 
-            if (colour === "とううん" && number === 0) {
+            if (colour === "とううん" ) {
                 result = "残念…";
-            } else if (colour === "しののめ" && number === 0) {
+            } else if (colour === "しののめ" ) {
                 result = "正解！！";
-            } else if (colour === "ひがしくも" && number === 0) {
+            } else if (colour === "ひがしくも" ) {
                 result = "ざんねん…";
-            } else if (colour === "とうのめ" && number === 0) {
+            } else if (colour === "とうのめ" ) {
                 result = "惜しい！";
             } else 
 
-const prefecturalCapital = [
-    {
-      id: "01",
-      question: "北海道の県庁所在地は？",
-      answer01: "札幌",
-      answer02: "福島",
-      answer03: "前橋",
-      answer04: "秋田",
-    },
-    {
-      id: "02",
-      question: "青森県の県庁所在地は？",
-      answer01: "青森",
-      answer02: "前橋",
-      answer03: "秋田",
-      answer04: "札幌",
-    },
-    {
-      id: "03",
-      question: "岩手県の県庁所在地は？",
-      answer01: "盛岡",
-      answer02: "福島",
-      answer03: "仙台",
-      answer04: "山形",
-    },
-    {
-      id: "04",
-      question: "宮城県の県庁所在地は？",
-      answer01: "仙台",
-      answer02: "札幌",
-      answer03: "前橋",
-      answer04: "水戸",
-    },
-    {
-      id: "05",
-      question: "秋田県の県庁所在地は？",
-      answer01: "秋田",
-      answer02: "盛岡",
-      answer03: "青森",
-      answer04: "札幌",
-    },
-  ];
-                
-            document.getElementById('result-output').innerText = "【" + result + "】";
-        }
 
-const time = document.getElementById('time');
-const startButton = document.getElementById('start');
-const stopButton = document.getElementById('stop');
-const resetButton = document.getElementById('reset');
-
-// 開始時間
-let startTime;
-// 停止時間
-let stopTime = 0;
-// タイムアウトID
-let timeoutID;
-
-// 時間を表示する関数
-function displayTime() {
-  const currentTime = new Date(Date.now() - startTime + stopTime);
-  const h = String(currentTime.getHours()-1).padStart(2, '0');
-  const m = String(currentTime.getMinutes()).padStart(2, '0');
-  const s = String(currentTime.getSeconds()).padStart(2, '0');
-
-
-  time.textContent = `${h}:${m}:${s}.${ms}`;
-  timeoutID = setTimeout(displayTime, 10);
-}
-
-// スタートボタンがクリックされたら時間を進める
-startButton.addEventListener('click', () => {
-  startButton.disabled = true;
-  stopButton.disabled = false;
-  resetButton.disabled = true;
-  startTime = Date.now();
-  displayTime();
-});
-
-// ストップボタンがクリックされたら時間を止める
-stopButton.addEventListener('click', function() {
-  startButton.disabled = false;
-  stopButton.disabled = true;
-  resetButton.disabled = false;
-  clearTimeout(timeoutID);
-  stopTime += (Date.now() - startTime);
-});
-
-// リセットボタンがクリックされたら時間を0に戻す
-resetButton.addEventListener('click', function() {
-  startButton.disabled = false;
-  stopButton.disabled = true;
-  resetButton.disabled = true;
-  time.textContent = '00:00:00';
-  stopTime = 0;
-});
 
 
 
