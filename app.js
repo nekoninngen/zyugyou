f4879d3dca18481cc23c33aa06dfe894b78acedf
nekoninngen <sa2im3oc@gmail.com>
2024-12-09 13:45:09 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-        function checkFortune() {
-            var colour = document.getElementById('colour-select').value;
-            var number = parseInt(document.getElementById('number-input').value, 10);
-            var result = "回答してね"; // デフォルトの値
-
-            if (colour === "とううん" ) {
-                result = "残念…";
-            } else if (colour === "しののめ" ) {
-                result = "正解！！";
-            } else if (colour === "ひがしくも" ) {
-                result = "ざんねん…";
-            } else if (colour === "とうのめ" ) {
-                result = "惜しい！";
-            } else 
-                    
-
-
 const quiz = [
     {
         question: 'Q1. 漫画「ナルト」に登場した虫を使う戦闘スタイルの忍び一族は？',
@@ -160,12 +143,3 @@ while(handlerIndex < buttonLength) {
     });
     handlerIndex++;
 }
-                    
-
-document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
-        }
-
-
-
-
-
